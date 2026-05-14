1d2ef998581ab04186b73c49c492052258ee7973
Westbrook <504899804@qq.com>
2026-05-14 08:46:12 +0800

    初始化项目

---
+++ Source/LyraEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class LyraEditorTarget : TargetRules
+{
+	public LyraEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("Lyra");
+	}
+}
